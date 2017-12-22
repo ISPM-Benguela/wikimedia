6fd15471ab3073508bbc5bdd4ffe471552b5b8fe
tandavala <tandavala@github.com>
2017-12-22 19:13:39 +0100

    initial commit

---
+++ wikimedia/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wikimedia project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wikimedia.settings")
+
+application = get_wsgi_application()
